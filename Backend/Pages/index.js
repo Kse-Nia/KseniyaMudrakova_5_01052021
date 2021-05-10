d1574d10aa9daf6af9e19b43356fee5b60f91265
Kseniya <kseniyamudrakovadev@gmail.com>
2021-05-10 21:06:40 +0200

    ajout Bootstrap

---
+++ Backend/Pages/index.js
@@ -0,0 +1,14 @@
+main ();
+
+function main(){
+    const article = getArticles();
+    displayArticles(articles);
+}
+
+function getArticles(){
+    return "";
+}
+
+function displayArticles(){
+    return ""
+}
