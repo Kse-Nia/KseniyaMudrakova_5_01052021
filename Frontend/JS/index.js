03da4d12c36a9fd13e928d673a00418847a7c8c5
Kseniya <kseniyamudrakovadev@gmail.com>
2021-05-28 00:48:50 +0200

    test modif index js

--- Frontend/JS/index.js
+++ Frontend/JS/index.js
@@ -1,11 +1,17 @@
 const docHtml = document.getElementById("main");
 fetch("http://localhost:3000/api/cameras")
-.then(Response => Response.json())
-.then(data => {
-console.log(data);
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
     console.log(data[0]);
     console.log(data[0].name);
-    let camera = document.createElement("h2")
+    let camera = document.createElement("h2");
     camera.textContent = data[0].name;
     docHtml.appendChild(camera);
-})
+
+    console.log(data[0].price);
+    let price = document.createElement("price");
+    price.textContent = data[0].price;
+    docHtml.appendChild(price);
+
+  });
