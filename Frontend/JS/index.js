ef5a5861e335c34f809a1f846267ae6b769bff6e
Kseniya <kseniyamudrakovadev@gmail.com>
2021-05-28 14:01:35 +0200

    progression ajout

--- Frontend/JS/index.js
+++ Frontend/JS/index.js
@@ -1,11 +1,27 @@
 const docHtml = document.getElementById("main");
 fetch("http://localhost:3000/api/cameras")
-.then(Response => Response.json())
-.then(data => {
-console.log(data);
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
     console.log(data[0]);
+
     console.log(data[0].name);
-    let camera = document.createElement("h2")
+    const camera = document.createElement("h2");
     camera.textContent = data[0].name;
     docHtml.appendChild(camera);
-})
+
+    console.log(data[0].price);
+    const price = document.createElement("div");
+    price.textContent = data[0].price;
+    docHtml.appendChild(price);
+
+    console.log(data[0].description);
+    const description = document.createElement("div");
+    description.textContent = data[0].description;
+    docHtml.appendChild(description);
+
+    console.log(data[0].imageUrl);
+    const picture = document.createElement("img");
+    picture.innerHTML = `<img src="http://localhost:3000/images/vcam_1.jpg"/>`;
+    docHtml.appendChild(picture);
+  });
