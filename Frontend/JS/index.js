0811ad413b7ac908b6d6858760555ce8d31bc82b
Kseniya <kseniyamudrakovadev@gmail.com>
2021-05-28 18:24:58 +0200

    ajout image

--- Frontend/JS/index.js
+++ Frontend/JS/index.js
@@ -5,13 +5,22 @@ fetch("http://localhost:3000/api/cameras")
     console.log(data);
     console.log(data[0]);
     console.log(data[0].name);
-    let camera = document.createElement("h2");
+    const camera = document.createElement("h2");
     camera.textContent = data[0].name;
     docHtml.appendChild(camera);
 
     console.log(data[0].price);
-    let price = document.createElement("price");
+    const price = document.createElement("div");
     price.textContent = data[0].price;
     docHtml.appendChild(price);
 
+    console.log(data[0].description);
+    const description = document.createElement("div");
+    description.textContent = data[0].description;
+    docHtml.appendChild(description);
+
+    const img = new Image(); 
+    img.src = "http://localhost:3000/images/vcam_1.jpg";
+    img.alt = "alt";
+    document.body.appendChild(img);
   });
