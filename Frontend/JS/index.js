5a9a6e628f821c53b9feb596d75944ecc47cfcff
Kseniya <kseniyamudrakovadev@gmail.com>
2021-06-09 16:04:05 +0200

    creation template index

--- Frontend/JS/index.js
+++ Frontend/JS/index.js
@@ -3,6 +3,7 @@ fetch("http://localhost:3000/api/cameras")
   .then((response) => response.json())
   .then((data) => {
     for (let i = 0; i < data.length; i++) {
+  /* 
       const camera = document.createElement("h2");
       camera.textContent = data[i].name;
       docHtml.appendChild(camera);
@@ -20,8 +21,17 @@ fetch("http://localhost:3000/api/cameras")
       img.width = "300";
       img.setAttribute("alt", data[i].name);
       docHtml.appendChild(img);
+*/
 
+      docHtml.innerHTML += `
+      <div class="article">
+      <a href="produit.html?id=${data[i]._id}" />
+      <h3 class="name">${data[i].name}</h3>
+      <img src=${data[i].imageUrl} alt="" class="image">
+      <p class="info">${data[i].description}</p>
+      <p class="prix">${data[i].price} €</p>
+      </a>
+  </div>
+      `;
     }
   });
-
-  
--- Frontend/JS/produit.js
+++ Frontend/JS/produit.js
@@ -1,5 +1,11 @@
-let params = (new URL(document.location)).searchParams;
-let id = params.get('id');
+let params = new URL(document.location).searchParams;
+let id = params.get("id");
 console.log(id);
 
+fetch(`http://localhost:3000/api/cameras/${id}`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
+  
 // http://127.0.0.1:5501/Frontend/produit.html?id=12345
