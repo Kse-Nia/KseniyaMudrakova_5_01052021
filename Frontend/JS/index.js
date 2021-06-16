178b0fa3f8086142415a6fadca85ba46fb708984
Kseniya <kseniyamudrakovadev@gmail.com>
2021-06-16 19:22:45 +0200

    modif bootstrap bis

--- Frontend/JS/index.js
+++ Frontend/JS/index.js
@@ -1,4 +1,7 @@
+// Définir les variables
+
 const docHtml = document.getElementById("container");
+
 fetch("http://localhost:3000/api/cameras")
   .then((response) => response.json())
   .then((data) => {
@@ -20,21 +23,22 @@ fetch("http://localhost:3000/api/cameras")
       img.setAttribute("src", data[i].imageUrl);
       img.width = "300";
       img.setAttribute("alt", data[i].name);
-      docHtml.appendChild(img);
-*/
-
+      docHtml.appendChild(img); */
+      
       docHtml.innerHTML += `
-      <div class="card d-flex style="width: 18rem;">
+      <div class="card  text-dark" style="background-color:#f0f1ff;">
+      <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
       <a href="produit.html?id=${data[i]._id}" />
       <img src=${data[i].imageUrl} class="card-img-top img-thumbnail" alt="présentation de l'appareil photo">
+      <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
       <div class="card-body text-center">
         <h3 class="card-title">${data[i].name}</h3>
         <p class="card-text text-dark">${data[i].description}</p>
-        <p class="card-text text-dark">${data[i].price} € </p>
+        <p class="card-text text-dark fw-bolder">${data[i].price} € </p>
       </div>
       </a>
+      </div>
     </div>
-
       `;
     }
   });
--- Frontend/JS/produit.js
+++ Frontend/JS/produit.js
@@ -1,15 +1,16 @@
 let params = new URL(document.location).searchParams;
 let id = params.get("id");
-console.log(id);
 
-const docHtml = document.getElementsByClassName("produitContent");
+let docHtml = document.getElementsByClassName("produitContent");
+let selecChoice = document.getElementsByClassName("select-box");
 
 fetch(`http://localhost:3000/api/cameras/${id}`)
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
+
     docHtml[0].innerHTML += `
-      <div class="article2">
+      <div class=" container article2">
       <div class="card d-flex text-center" style="width: 30rem;">
         <a href="produit.html?id=${data._id}" />
         <h3 class="card-title">${data.name}</h3>
@@ -20,14 +21,12 @@ fetch(`http://localhost:3000/api/cameras/${id}`)
         <p class="price">${data.price} €</p>
         </div>
         <div class="select-box">
-        <label for="lenses-select">Choisir votre lentille</label>
-        <select name="choice" id="lense-select">
-            <option value="lense" id="lensechoice">${data.lenses}</option>
-        </select>
+        <option value = "lense"></option>
+        <input class="btn btn-primary" type="submit" value="Valider">
         </div>
        </a>
       </div>
-      `;
-  });
-
+        `; 
+          })
 // http://127.0.0.1:5501/Frontend/produit.html?id=12345
+
