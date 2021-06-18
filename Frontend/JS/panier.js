e052beab8cfd79ead3b1c2959bbd750db2c8cfac
Kseniya <kseniyamudrakovadev@gmail.com>
2021-06-18 15:39:33 +0200

    modif bootstrap

--- Frontend/JS/panier.js
+++ Frontend/JS/panier.js
@@ -1,3 +1,9 @@
-function addProduct(
-    description, 
-)
+// Variable ajout produit
+
+let cartStorage = JSON.parse(localStorage.getItem("produit"));
+
+if (cartStorage == null){
+    cartStorage = [];
+    cartStorage.push(cart)
+    localStorage.setItem("produit", JSON.stringify(cartStorage))
+}
--- Frontend/JS/produit.js
+++ Frontend/JS/produit.js
@@ -13,18 +13,21 @@ fetch(`http://localhost:3000/api/cameras/${id}`)
     <div class="card  text-dark" style="background-color:#f0f1ff;">
     <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
     <a href="produit.html?id=${data._id}" />
-    <img src=${data.imageUrl} class="card-img-top img-thumbnail " alt="présentation de l'appareil photo">
+    <img src=${
+      data.imageUrl
+    } class="card-img-top img-thumbnail " alt="présentation de l'appareil photo">
     <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
     <div class="card-body text-center">
       <h3 class="card-title">${data.name}</h3>
       <p class="card-text text-dark">${data.description}</p>
       <p class="card-text text-dark fw-bolder">${data.price} € </p>
-      <div class="select-box">
+      <div class="select-box d-flex flex-column ">
       <select name="selection" id="lense">
-  
-      ${data.lenses.map(lense => `<option value=${lense} id=${data.id}>${lense}</option>`)}
+      ${data.lenses.map(
+        (lense) => `<option value=${lense} id=${data.id}>${lense}</option>`
+      )}
 
-      <input class="btn btn-primary" type="submit" value="Valider">
+      <input class="btn btn-primary mt-3" type="submit" value="Valider">
       </select>
       </div>
     </div>
@@ -32,14 +35,15 @@ fetch(`http://localhost:3000/api/cameras/${id}`)
     </div>
   </div>
         `;
+  
+  });
 
-    this.lenses = data.lenses;
+// http://127.0.0.1:5501/Frontend/produit.html?id=12345
+/*
+this.lenses = data.lenses;
     for (let i = 0; i < lenses.lenght; i++) {
       selecChoice.innerHTML += `
             <option value = "lense">${data[i].lenses}</option>
             <option value = "lense">${data[i].lenses}</option>
-    
             `;
-    }
-  });
-// http://127.0.0.1:5501/Frontend/produit.html?id=12345
+    */
