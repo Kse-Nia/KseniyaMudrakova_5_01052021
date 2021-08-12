ae8d4309a6aafded00410366a6fa624fb64f1845
Kseniya <kseniyamudrakovadev@gmail.com>
2021-08-12 18:56:10 +0200

    progression

--- Frontend/JS/panier.js
+++ Frontend/JS/panier.js
@@ -2,18 +2,18 @@
 
 let recup = localStorage.getItem("camera");
 let total = 0;
+let affichagePanier = document.getElementById('table');
+
+// Ajout code HTML Panier
+
+affichagePanier.innerHTML += `
+
+
+`
 
 // Recupération des produits du localStorage
 
-const totalCart = document.getElementsByClassName("total");
-const quantity = document.getElementsByClassName("quantity");
-const table = document.getElementsByClassName("table");
-console.log(localStorage);
 
 const camera = localStorage.getItem("camera");
 
 // Ajout des produits
-
-
-// Suppression du panier
-
